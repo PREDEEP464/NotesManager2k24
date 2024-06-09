3ec9b5df5f32a4970dca33fb1e8d564e40ebb9e3
Predeep <predeepkumar.us2022cse@sece.ac.in>
2024-06-09 18:43:47 +0530

    CORS

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cors(
     {
         origin: ["https://notes-manager2k24.vercel.app"],
-        methods: ["POST", "GET"],
+        methods: ["POST", "GET","PUT","DELETE"],
         credentials: true
     }
 ));
